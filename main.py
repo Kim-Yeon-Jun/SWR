06b3cc74ae847615593e675213e0a0533abec9fe
Kim-Yeon-Jun <106429088+Kim-Yeon-Jun@users.noreply.github.com>
2024-04-01 12:08:20 +0900

    update requirements

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
+# 이미지 URL을 받아서 텍스트를 추출하여 JSON 형식으로 반환하는 엔드포인트
+@app.get("/detect/")
 async def extract_text_from_image(image_url: str):
     try:
         # 이미지 다운로드
@@ -11,12 +13,12 @@ async def extract_text_from_image(image_url: str):
         response.raise_for_status()  # HTTP 에러 발생 시 예외 발생
         
         # EasyOCR을 초기화하고 이미지에서 텍스트 추출
-        reader = easyocr.Reader(['ko'])  # 영어 텍스트 추출을 위해 'en' 지정, 다른 언어도 가능
+        reader = easyocr.Reader(['ko'])  
         results = reader.readtext(response.content)
         
         # 추출된 텍스트 정보를 JSON 형식으로 반환
         extracted_text = [{"text": result[1], "confidence": result[2]} for result in results]
-        
+        print(result for result in results)
         return {"image_url": image_url, "extracted_text": extracted_text}
     
     except requests.exceptions.RequestException as e:
